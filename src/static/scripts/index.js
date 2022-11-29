906f91dfa3dbf84e55619595728bdbc57bb23428
Edvard Høiby <EdvardHoiby@outlook.com>
2022-11-29 10:12:47 +0100

    feat(static): add logic for background image and some style changes (#7)
    Co-authored-by: Edvard Høiby <edvard.hoiby@schibsted.com>
    Co-authored-by: Johannes Andersen <johannes.andersen@schibsted.com>

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,13 +1,40 @@
 console.log("Hello from frontend");
 
+const backgroundHandler = () => {
+  const imageNames = [
+    "dessert-cave-background.png",
+    "glitter-background.png",
+    "lagoon-fist-beach-background.png",
+    "forrest-background.png",
+    "dessert-stones-background.png",
+    "paint-flow-background.png",
+    "glitter-2-background.png",
+  ];
+  const imageRootPath = "static/images/";
+
+  const index = Number(localStorage.getItem("backgroundIndex") || 0);
+
+  const nextIndex = (index + 1) % imageNames.length;
+
+  localStorage.setItem("backgroundIndex", nextIndex);
+
+  document.body.style.backgroundImage = `url(${imageRootPath}${imageNames[nextIndex]})`;
+};
+
+backgroundHandler();
+
 const dropHandler = (event) => {
   console.log("File dropped", event);
 
   event.preventDefault();
+
+  const files = event.dataTransfer.files;
+
+  console.log(files);
 };
 
 const dragOverHandler = (event) => {
-  console.log("File dragged over", event);
-
   event.preventDefault();
+
+  // console.log(backgroundImage);
 };
