d9d9dc61a2019a73345bca34804b5e391af8d0ad
Edvard Høiby <edvard.hoiby@schibsted.com>
2022-11-30 16:38:27 +0100

    feat(static): drop graphic as html and css improvements

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -1,16 +1,68 @@
 console.log("Hello from frontend");
 
+const imageRootPath = "static/images/";
+
+let backgroundImage = "";
+
+const getInputContainer = () => document.getElementById("input-container");
+const getForm = () => document.getElementById("form");
+const getDropGraphic = () => document.getElementById("drop-graphic");
+const getImageCreditElement = () => document.getElementById("image-credit");
+const getFileUploadButton = () => document.getElementById("file-upload-button");
+
+const setBackgroundImage = (image) => {
+  document.body.style.backgroundImage = `url(${imageRootPath}${image.name})`;
+
+  getImageCreditElement().innerHTML = `
+      ${image.author} on ${image.origin}
+  `;
+};
+
 const backgroundHandler = () => {
   const imageNames = [
-    "dessert-cave-background.png",
-    "glitter-background.png",
-    "lagoon-fist-beach-background.png",
-    "forrest-background.png",
-    "dessert-stones-background.png",
-    "paint-flow-background.png",
-    "glitter-2-background.png",
+    {
+      name: "dessert-cave-background.png",
+      author: "Joshua Earle",
+      origin: "Unsplash",
+      accentcolor: "#8b5e56",
+    },
+    {
+      name: "glitter-background.png",
+      author: "Joshua Earle",
+      origin: "Unsplash",
+      accentcolor: "#835d6b",
+    },
+    {
+      name: "lagoon-fist-beach-background.png",
+      author: "Nathan Dumlao",
+      origin: "Unsplash",
+      accentcolor: "#8bbcba",
+    },
+    {
+      name: "forrest-background.png",
+      author: "Kaique Rocha",
+      origin: "Unsplash",
+      accentcolor: "#354c33",
+    },
+    {
+      name: "dessert-stones-background.png",
+      author: "Jacek Dylag",
+      origin: "Unsplash",
+      accentcolor: "#b68a7e",
+    },
+    {
+      name: "paint-flow-background.png",
+      author: "Jannis Brandt",
+      origin: "Unsplash",
+      accentcolor: "#453953",
+    },
+    {
+      name: "glitter-2-background.png",
+      author: "Eddie Kopp",
+      origin: "Unsplash",
+      accentcolor: "#775d5d",
+    },
   ];
-  const imageRootPath = "static/images/";
 
   const index = Number(localStorage.getItem("backgroundIndex") || 0);
 
@@ -18,23 +70,72 @@ const backgroundHandler = () => {
 
   localStorage.setItem("backgroundIndex", nextIndex);
 
-  document.body.style.backgroundImage = `url(${imageRootPath}${imageNames[nextIndex]})`;
+  backgroundImage = imageNames[nextIndex];
+  setBackgroundImage(backgroundImage);
 };
 
-backgroundHandler();
+const init = () => {
+  backgroundHandler();
 
-const dropHandler = (event) => {
-  console.log("File dropped", event);
+  document.body.style.backgroundColor = backgroundImage.accentcolor;
+  getFileUploadButton().style.backgroundColor = backgroundImage.accentcolor;
+};
 
-  event.preventDefault();
+init();
 
-  const files = event.dataTransfer.files;
+const createDropGraphic = () => {
+  const dropGraphicElement = document.createElement("div");
+  dropGraphicElement.id = "drop-graphic";
+  dropGraphicElement.classList.add("drop-graphic");
+  dropGraphicElement.innerHTML = `
+    <div class="create-graphic-text">
+      <span>Drop it like it's hot</span>
+    </div>
+  `;
 
-  console.log(files);
+  return dropGraphicElement;
 };
 
 const dragOverHandler = (event) => {
   event.preventDefault();
+};
 
-  // console.log(backgroundImage);
+const dragStartHandler = (event) => {
+  console.log("Drag started", event);
+
+  if (!getDropGraphic()) {
+    document.body.style.backgroundImage = "none";
+    getImageCreditElement().style.display = "none";
+    getInputContainer().style.display = "none";
+    document.getElementById("main").appendChild(createDropGraphic());
+  }
+};
+
+const endFileHover = () => {
+  setBackgroundImage(backgroundImage);
+
+  const dropGraphic = getDropGraphic();
+  dropGraphic.remove();
+
+  getInputContainer().style.display = "flex";
+  getImageCreditElement().style.display = "block";
+};
+
+const dragEndHandler = (event) => {
+  console.log("Drag ended", event);
+
+  endFileHover();
+};
+
+const dropHandler = (event) => {
+  event.preventDefault();
+  console.log("File dropped", event);
+
+  endFileHover();
+};
+
+const formChangeHandler = (event) => {
+  // console.log("Form changed", event);
+
+  event.preventDefault();
 };
