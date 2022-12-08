d8e8e309956e3541315a1a20121fe1ee5b649a75
Edvard Høiby <edvard.hoiby@schibsted.com>
2022-12-08 16:13:32 +0100

    feat: use enviroment variables

--- src/mailer.py
+++ src/mailer.py
@@ -1,21 +1,23 @@
 import smtplib
+import os
 
 def send_mail(recipient, subject, body):
   # Sender email and password
-  user = 'jojo@vg.no'
-  password = '1234'
+  email_sender_address = os.environ.get('EMAIL_SENDER_ADDRESS')
+  email_sender_password = os.environ.get('EMAIL_SENDER_PASSWORD')
 
   # Connect to the Gmail SMTP server
-  smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+  email_sender_host = os.environ.get('EMAIL_SENDER_HOST')
+  smtp_server = smtplib.SMTP_SSL(email_sender_host, 465)
 
   # Login to the Gmail server
-  smtp_server.login(user, password)
+  smtp_server.login(email_sender_address, email_sender_password)
 
   # Construct the email message
   message = f'Subject: {subject}\n\n{body}'
 
   # Send the email
-  smtp_server.sendmail(user, recipient, message)
+  smtp_server.sendmail(email_sender_address, recipient, message)
 
   # Disconnect from the server
   smtp_server.quit()
@@ -24,7 +26,9 @@ def send_success_email(job, connection, result, *args, **kwargs):
   email = job.meta.get('email')
   subject = 'Your file is ready'
 
-  download_url = "http://127.0.0.1:3000/v1/download/" + job.id
+  base_url = os.environ.get('BASE_URL')
+
+  download_url = base_url + "/v1/download/" + job.id
   body = f'Your file is ready. Download it here: {download_url}'
 
   send_mail(email, subject, body)
