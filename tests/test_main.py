6b5f5e8f803a5532c313f64cc0ad6567d27f2856
Johannes Andersen <johannes.andersen@schibsted.com>
2022-12-09 08:50:06 +0100

    test: fix failing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,11 +3,13 @@ import whisper
 
 from src import app
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
 
     return app.test_client()
+
 
 def test_detect_options(client):
     response = client.options('/v1/detect')
@@ -23,6 +25,7 @@ def test_detect_options(client):
             },
         }
     }
+
 
 def test_transcribe_options(client):
     response = client.options('/v1/transcribe')
@@ -41,25 +44,21 @@ def test_transcribe_options(client):
                 "options": ["translate", "transcribe"],
                 "optional": True,
                 "default": "transcribe",
-            },                
+            },
             "languages": {
                 "type": "enum",
                 "options": list(whisper.tokenizer.LANGUAGES.values()),
                 "optional": True,
             },
-            "output": {
-                "type": "enum",
-                "options": ["srt", "vtt", "json", "txt"],
-                "optional": True,
-                "default": "srt",
-            },
         }
     }
+
 
 def test_download_not_found(client):
     response = client.get('/v1/download/1')
 
     assert response.status_code == 404
+
 
 def test_queue(client):
     response = client.get('/v1/queue')
