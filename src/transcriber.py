6dc02e7ab766f41f8bfc254fc4524e5f0e066685
Audun Ytterdal <audun.ytterdal@vg.no>
2022-12-21 14:13:02 +0100

    Wrong name

--- src/transcriber.py
+++ src/transcriber.py
@@ -2,7 +2,7 @@ import whisper
 
 def transcribe(filename, requestedModel, task, language,email):
     # Mail is not used here, but it makes it easier for the worker to log mail
-    print("Executing transcribing of " + filename + " for "+ email + " using " + model + " model ")
+    print("Executing transcribing of " + filename + " for "+ email + " using " + requestedModel + " model ")
     model = whisper.load_model(requestedModel)
 
     return model.transcribe(filename, language=language, task=task)
