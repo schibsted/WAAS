6bc7b2db466b5be6474008598e6d4a3946d3e82f
Audun Ytterdal <audun@ytterdal.net>
2022-12-21 14:11:00 +0100

    Adding mail to the request for logging (#58)
    Co-authored-by: Johannes Andersen <johannes.andersen@schibsted.com>

--- src/transcriber.py
+++ src/transcriber.py
@@ -1,6 +1,8 @@
 import whisper
 
-def transcribe(filename, requestedModel, task, language):
+def transcribe(filename, requestedModel, task, language,email):
+    # Mail is not used here, but it makes it easier for the worker to log mail
+    print("Executing transcribing of " + filename + " for "+ email + " using " + model + " model ")
     model = whisper.load_model(requestedModel)
 
     return model.transcribe(filename, language=language, task=task)
