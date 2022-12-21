66c1b4babfdeb93af6a7509acf21387618b7a632
Audun Ytterdal <audun.ytterdal@vg.no>
2022-12-21 14:27:31 +0100

    Adding set_user to worker

--- src/transcriber.py
+++ src/transcriber.py
@@ -1,8 +1,9 @@
 import whisper
+from sentry_sdk import set_user
 
 def transcribe(filename, requestedModel, task, language,email):
     # Mail is not used here, but it makes it easier for the worker to log mail
     print("Executing transcribing of " + filename + " for "+ email + " using " + requestedModel + " model ")
+    set_user({"email": email})
     model = whisper.load_model(requestedModel)
-
     return model.transcribe(filename, language=language, task=task)
