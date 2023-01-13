b45b3f0837059ab53e1495c1c6c7c7c00f68792d
Ole-Kenneth Bratholt <ok@schibsted.com>
2023-01-13 11:40:47 +0100

    return file as attachment link instead of JSON

--- src/main.py
+++ src/main.py
@@ -246,8 +246,14 @@ def download(job_id):
             if output == "json":
                 return job.result
             if output == "jojo":
-                doc = generate_jojo_doc(filename, job.result["segments"])
-                return doc
+                return Response(
+                    generate_jojo_doc(filename, job.result["segments"]),
+                    mimetype="application/json",
+                    headers={
+                        'Content-disposition': f'attachment; filename="{filename}.jojo"'
+                    },
+                    status=200
+                )
             if output == "vtt":
                 return Response(
                     generate_vtt(job.result["segments"]),
