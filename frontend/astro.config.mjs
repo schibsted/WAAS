589e814c27273690f3c9b70c52000a5f987b22cb
Johannes Andersen <jotoffen@gmail.com>
2023-12-22 01:02:19 +0100

    feat: start moving landing page styles over

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,8 +3,12 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jojo.schibsted.com",
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
+  prefetch: {
+    prefetchAll: true,
+  },
 });
---
+++ frontend/src/components/BackgroundImages/credits.ts
@@ -0,0 +1,57 @@
+import boliviainteligente from "./boliviainteligente.jpg";
+import kukurudziak from "./kukurudziak.jpg";
+import topuzoglu from "./topuzoglu.jpg";
+import tischler from "./tischler.jpg";
+import grobelska from "./grobelska.jpg";
+import horvath from "./topuzoglu.jpg";
+import jehly from "./jehly.jpg";
+
+export const images: Array<{
+  image: any;
+  author: string;
+  origin: string;
+  accentColor: string;
+}> = [
+  {
+    image: boliviainteligente,
+    author: "BoliviaInteligente",
+    origin: "Unsplash",
+    accentColor: "#361b2d",
+  },
+  {
+    image: kukurudziak,
+    author: "Max Kukurudziak",
+    origin: "Unsplash",
+    accentColor: "#514b3e",
+  },
+  {
+    image: topuzoglu,
+    author: "Tansu Topuzoğlu",
+    origin: "Unsplash",
+    accentColor: "#42382b",
+  },
+  {
+    image: tischler,
+    author: "Geio Tischler",
+    origin: "Unsplash",
+    accentColor: "#334749",
+  },
+  {
+    image: grobelska,
+    author: "Mariola Grobelska",
+    origin: "Unsplash",
+    accentColor: "#722a3d",
+  },
+  {
+    image: horvath,
+    author: "Richard Horvath",
+    origin: "Unsplash",
+    accentColor: "#832f28",
+  },
+  {
+    image: jehly,
+    author: "Nicolas Jehly",
+    origin: "Unsplash",
+    accentColor: "#40422e",
+  },
+];
